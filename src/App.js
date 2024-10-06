de08cd42e741867077682355002d0e61ad46a110
qkrwnstn356 <qkrwnstn356@naver.com>
2024-10-06 14:51:55 +0900

    add1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Main from './components/Main';
 import Footer from './components/Footer';
 
 function App() {
+
+  const BodyProps = {
+    name: "박준수",
+    location: "용인시",
+    favorList: [
+      {name : "HTML", gender : '남자',id : 0},
+      {name : "JS", gender : '여자',id : 1}, 
+    ],
+  };
+
   return (
     <div className="App">
       <Header></Header>
-      <Main></Main>
+      <Main {...BodyProps}></Main>
       <Footer></Footer>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,17 @@
 // import {useState} from 'react'
+import utils from '../utils'
+import {useState} from 'react'
+const Main = (BodyProps) => {
+  console.log('데이터 테스트 ==>', utils.clone(BodyProps));
+  const [state, setState] = useState("박준수");
 
-const Main = () => {
-    return (
-        <div>
-            <p>test</p>
-        </div>
-    );
+  const handleName = (e) => { setState(e.currentTarget.value); }
+
+  return (<>
+      <div>
+          <input name="name" value={state} onChange={handleName} placeholder="이름"/>
+      </div>
+  </>);
 }
 
 export default Main;
